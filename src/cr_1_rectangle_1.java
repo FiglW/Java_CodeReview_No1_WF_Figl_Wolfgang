26db3ad2bc4d5b7710e7928b5c326f68754cc52c
Wolfgang Figl <wolfgang.figl@me.com>
2019-11-22 10:05:41 +0100

    last update with cm²

--- src/cr_1_rectangle_1.java
+++ src/cr_1_rectangle_1.java
@@ -12,23 +12,23 @@ public class cr_1_rectangle_1 {
 
         System.out.println("The rectangularArea is: "+rectangularArea+"m");
 
-        System.out.println("Rectangle 20x30 has a area of: "+calculateRectArea(20, 30));
-        System.out.println("Rectangle 30x40 has a area of: "+calculateRectArea(30,40));
+        System.out.println("Rectangle 20x30 has a area of: "+calculateRectArea(20, 30)+"cm²");
+        System.out.println("Rectangle 30x40 has a area of: "+calculateRectArea(30,40)+"cm2");
 
 
         //POINT 5
 
-        System.out.println("Rectangle 20x30 has a area of: "+calculateRectArea(50, 80));
-        System.out.println("Rectangle 30x40 has a area of: "+calculateRectArea(60,90));
-        System.out.println("Rectangle 20x30 has a area of: "+calculateRectArea(120, 150));
-        System.out.println("Rectangle 30x40 has a area of: "+calculateRectArea(200,300));
+        System.out.println("Rectangle 20x30 has a area of: "+calculateRectArea(50, 80)+"cm2");
+        System.out.println("Rectangle 30x40 has a area of: "+calculateRectArea(60,90)+"cm2");
+        System.out.println("Rectangle 20x30 has a area of: "+calculateRectArea(120, 150)+"cm2");
+        System.out.println("Rectangle 30x40 has a area of: "+calculateRectArea(200,300)+"cm2");
 
 
         //Bonus Point 1
         int [] arr1 = {6,8,10,15};
         int [] arr2 = { 10,14,18,20};
         for (int i = 0; i < arr1.length; i++){
-            System.out.println("The Area if this Area is: "+i+ "is"+ calculateRectArea(arr1[i],arr2[i]));
+            System.out.println("The Area if this Area is: "+i+ "is "+ calculateRectArea(arr1[i],arr2[i])+"cm2");
         }
 
 
